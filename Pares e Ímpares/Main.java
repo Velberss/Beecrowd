1a15a9f00a6593e1c8125aad7be59ddae19b06e4
Velber <goncalvesvelber@gmail.com>
2024-11-20 10:58:12 -0300

    more changes

--- Pares e Ímpares/Main.java
+++ Pares e Ímpares/Main.java
@@ -1,57 +1,39 @@
+
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.IOException;
+import java.util.Collections;
+public class Main{
 
-public class Main {
 
     public static void main(String[] args) throws IOException {
-        
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Primeira entrada: ");
-        int numLinhas = sc.nextInt();
-        int entradas[] = new int[numLinhas];
-        int aux = 0;
-        int entradasPar[] = new int[numLinhas];
-        int entradasImpar[] = new int[numLinhas];
-        int auxPar = 0;
-        int auxImpar = 0;
 
-        for (int i = 0; i < numLinhas; i++) {
-            System.out.println("Informe a " + (i + 1) + " entrada: ");
-            entradas[i] = sc.nextInt();
-        }
+        ArrayList<Integer> impares = new ArrayList<>();
+        ArrayList<Integer> pares = new ArrayList<>();
 
-        for (int i = 0; i < entradas.length; i++) {
-            if (entradas[i] % 2 == 0) {
-                entradasPar[auxPar] = entradas[i];
-                auxPar++;
-            } else {
-                entradasImpar[auxImpar] = entradas[i];
-                auxImpar++;
+        int n = sc.nextInt();
+        int x;
+
+        while(n-->0){
+            x = sc.nextInt();
+            if(x%2 == 0) {
+                pares.add(x);
             }
+            else {
+                impares.add(x);}
         }
 
-        for (int i = 0; i < auxPar - 1; i++) {
-            for (int j = 0; j < auxPar - i - 1; j++) {
-                if (entradasPar[j] > entradasPar[j + 1]) {
-                    aux = entradasPar[j];
-                    entradasPar[j] = entradasPar[j + 1];
-                    entradasPar[j + 1] = aux;
-                }
-                if (entradasImpar[j] < entradasImpar[j + 1]) {
-                    aux = entradasImpar[j];
-                    entradasImpar[j] = entradasImpar[j + 1];
-                    entradasImpar[j + 1] = aux;
-                }
-            }
-        }
+        Collections.sort(pares);
+        Collections.sort(impares);
 
-        System.out.println();
-        for (int i = 0; i < auxPar; i++) {
-            System.out.println(entradasPar[i]);
-        }
+        for(int y=0; y < pares.size() ; y++) 
 
-        for (int i = 0; i < auxImpar; i++) {
-            System.out.println(entradasImpar[i]);
-        }
-    } 
+        System.out.println(pares.get(y));
+        for(int y = impares.size()-1 ; y>=0 ; y--) 
+        System.out.println(impares.get(y));
+        
+        sc.close();
+    }
 }
