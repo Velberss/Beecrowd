e349c65fc6e8ada86aff849a0f732bff5ea3cb64
Velber <goncalvesvelber@gmail.com>
2024-11-25 16:35:30 -0300

    tamo caminhando

--- Tabelas Hash/Main.java
+++ Tabelas Hash/Main.java
@@ -1,8 +1,21 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("");
+        int quantTestes = sc.nextInt();
+        sc.nextLine(); 
+
+        while (quantTestes-- > 0) {
+            System.out.println("");
+            String input = sc.nextLine();
+
+            String[] parts = input.trim().split("\\s+");
+
+            int m = Integer.parseInt(parts[0]);
+            int c = Integer.parseInt(parts[1]);
+            System.out.println(m + "  " + c);
+        }
     }
 }
