5d3e7d70c2be14b95055b6863d8f23efb829524f
Velber <goncalvesvelber@gmail.com>
2024-11-26 19:28:14 -0300

    progresso

--- Tabelas Hash/Main.java
+++ Tabelas Hash/Main.java
@@ -5,17 +5,40 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("");
         int quantTestes = sc.nextInt();
-        sc.nextLine(); 
+        sc.nextLine();
 
         while (quantTestes-- > 0) {
             System.out.println("");
             String input = sc.nextLine();
-
             String[] parts = input.trim().split("\\s+");
 
-            int m = Integer.parseInt(parts[0]);
-            int c = Integer.parseInt(parts[1]);
-            System.out.println(m + "" + c);
+            if (parts.length >= 2) {
+
+                int mMod = Integer.parseInt(parts[0]);
+                int cQuant = Integer.parseInt(parts[1]);
+
+                System.out.println(" " + mMod + " " + cQuant);
+
+                TabelaHash tabela = new TabelaHash(mMod);
+                List<Integer> chaves = Arrays.stream(input.trim().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+                for (int i = 0; i < chaves.size(); ++i) {
+                    tabela.adiciona(chaves.get(i));
+                }
+
+            } else {
+                System.out.println("Entrada inválida.");
+            }
+            sc.close();
+        }
+    }
+
+    public class TabelaHash {
+        private int chave;
+        private List<Integer>[] tabela;
+
+        public TabelaHash(int chave){
+            this.chave = chave;
+            this.tabela = new ArrayList[chave];
         }
     }
 }
