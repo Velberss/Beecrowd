7b8c02cdf5ac3ff934a311477e79e8af9ae36545
Velber <goncalvesvelber@gmail.com>
2024-12-06 23:12:14 -0300

    fazendo o catalogo

--- 1259 - Pares e Ímpares/Main.java
+++ 1259 - Pares e Ímpares/Main.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 import java.io.IOException;
 import java.util.Collections;
 public class Main{
-
-
     public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
@@ -28,12 +26,11 @@ public class Main{
         Collections.sort(pares);
         Collections.sort(impares);
 
-        for(int y=0; y < pares.size() ; y++) 
-
+        for(int y = 0; y < pares.size() ; y++) 
         System.out.println(pares.get(y));
+        
         for(int y = impares.size()-1 ; y>=0 ; y--) 
         System.out.println(impares.get(y));
-        
         sc.close();
     }
 }
--- 1802 - Catálago de Livros/Main.java
+++ 1802 - Catálago de Livros/Main.java
@@ -1,8 +1,31 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<List<Integer>> valores = new ArrayList<>(); // Cria o arraylist para colocas os valores
+        int maior = 0;
+        for (int i = 0; i < 5; i++) {
+            String linha = sc.nextLine(); // Recebe toda a linha para a formatação
+            String[] partes = linha.split(" "); // Separa toda a linha quando tem " "
+            int quantidadeLivros = Integer.parseInt(partes[0]); // Pega o primeiro valor
+            List<Integer> livros = new ArrayList<>(); // Cria o lista dos valores
+            for (int j = 1; j <= quantidadeLivros; j++) {
+                livros.add(Integer.parseInt(partes[j])); // Transforma em inteiro e passa pro livros
+            }
+            valores.add(livros);// adiciona a list no arraylist
+        }
+        for (List<Integer> sublista : valores) {
+            sublista.sort(Collections.reverseOrder()); // Converte a lista os valores para ordem descrescente
+        }
+        int k = sc.nextInt();
+
+        for (int i = 0; i > k; i++) {
+        }
+
         sc.close();
     }
 }
