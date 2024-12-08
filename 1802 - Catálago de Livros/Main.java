12428575f8924140febd1469825dfa3d60918e86
Velber <goncalvesvelber@gmail.com>
2024-12-08 10:57:42 -0300

    finalizei o catalogo

--- 1802 - Catálago de Livros/Main.java
+++ 1802 - Catálago de Livros/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<List<Integer>> valores = new ArrayList<>(); // Cria o arraylist para colocas os valores
-        int maior = 0;
+
         for (int i = 0; i < 5; i++) {
             String linha = sc.nextLine(); // Recebe toda a linha para a formatação
             String[] partes = linha.split(" "); // Separa toda a linha quando tem " "
@@ -21,11 +21,31 @@ public class Main {
         for (List<Integer> sublista : valores) {
             sublista.sort(Collections.reverseOrder()); // Converte a lista os valores para ordem descrescente
         }
+
         int k = sc.nextInt();
 
-        for (int i = 0; i > k; i++) {
+        List<Integer> somas = new ArrayList<>();
+        for (int a = 0; a < valores.get(0).size(); a++) {
+            for (int b = 0; b < valores.get(1).size(); b++) {
+                for (int c = 0; c < valores.get(2).size(); c++) {
+                    for (int d = 0; d < valores.get(3).size(); d++) {
+                        for (int e = 0; e < valores.get(4).size(); e++) {
+                            int soma = valores.get(0).get(a) + valores.get(1).get(b) + valores.get(2).get(c)
+                                    + valores.get(3).get(d) + valores.get(4).get(e);
+                            somas.add(soma); // Armazena a soma na lista
+                        }
+                    }
+                }
+            }
         }
 
+        somas.sort(Collections.reverseOrder());//Organizei todas as somas de forma decrescente
+        int somaFinal = 0;
+        
+        for (int i = 0; i < k; i++) {
+            somaFinal += somas.get(i);
+        }
+        System.out.println(somaFinal);
         sc.close();
     }
 }
